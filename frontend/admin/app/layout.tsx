51298de7abc21d6085ac4d1659e35ae1b6dc7092
mostcharming <mostcharming920@yahoo.com>
2025-10-21 09:51:50 +0100

    Font uodate for the admin

--- frontend/admin/app/layout.tsx
+++ frontend/admin/app/layout.tsx
@@ -1,13 +1,8 @@
 import { SidebarProvider } from "@/context/SidebarContext";
 import { ThemeProvider } from "@/context/ThemeContext";
-import { Outfit } from "next/font/google";
 import "simplebar-react/dist/simplebar.min.css";
 import "swiper/swiper-bundle.css";
 import "./globals.css";
-
-const outfit = Outfit({
-  subsets: ["latin"],
-});
 
 export default function RootLayout({
   children,
@@ -16,7 +11,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${outfit.className} dark:bg-gray-900`}>
+      <body className="dark:bg-gray-900">
         <ThemeProvider>
           <SidebarProvider>{children}</SidebarProvider>
         </ThemeProvider>
