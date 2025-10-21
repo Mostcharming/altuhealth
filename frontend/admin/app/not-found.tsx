9d572d70dba48c1cb09f9cacbf717babc1670c90
mostcharming <mostcharming920@yahoo.com>
2025-10-21 09:46:57 +0100

    Admin error Page

---
+++ frontend/admin/app/not-found.tsx
@@ -0,0 +1,47 @@
+import GridShape from "@/components/common/GridShape";
+import type { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "AltuHealth | Admin 404",
+  description: "404 Not Found Page",
+};
+
+export default function NotFound() {
+  return (
+    <div className="relative flex flex-col items-center justify-center min-h-screen p-6 overflow-hidden z-1">
+      <GridShape />
+      <div className="mx-auto w-full max-w-[242px] text-center sm:max-w-[472px]">
+        <Image
+          src="/images/error/404.svg"
+          alt="404"
+          className="pl-7 dark:hidden"
+          width={472}
+          height={152}
+        />
+        <Image
+          src="/images/error/404-dark.svg"
+          alt="404"
+          className="pl-3 hidden dark:block"
+          width={472}
+          height={152}
+        />
+
+        <p className="mt-10 mb-6 text-base text-gray-700 dark:text-gray-400 sm:text-lg">
+          Oops! The page you are looking for does not exist.
+        </p>
+
+        <Link
+          href="https://altuhealth.com/"
+          className="inline-flex items-center justify-center rounded-lg border border-gray-300 bg-white px-5 py-3.5 text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
+        >
+          Back to Home Page
+        </Link>
+      </div>
+      <p className="absolute text-sm text-center text-gray-500 -translate-x-1/2 bottom-6 left-1/2 dark:text-gray-400">
+        &copy; {new Date().getFullYear()} - AltuHealth
+      </p>
+    </div>
+  );
+}
