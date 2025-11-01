c3729697a53ad23f7631f3e44300bd3fec9b57bb
mostcharming <mostcharming920@yahoo.com>
2025-11-01 11:31:35 +0100

    Auth Middleare completed

--- frontend/admin/src/middleware.ts
+++ frontend/admin/src/middleware.ts
@@ -12,8 +12,6 @@ export function middleware(req: NextRequest) {
 
   const token = req.cookies.get("auth_token")?.value;
 
-  console.log("Middleware - Auth Token:", token);
-
   if (!token) {
     const signinUrl = new URL("/signin", req.url);
     signinUrl.searchParams.set("from", pathname);
@@ -24,5 +22,7 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico)$).*)",
+  ],
 };
