cb421576d24c3929f2f83e187f37dfb00c77b7d3
mostcharming <mostcharming920@yahoo.com>
2026-03-07 13:26:28 +0100

    Admin missing pages

--- frontend/admin/src/app/(main)/(others)/api-keys/page.tsx
+++ frontend/admin/src/app/(main)/(others)/api-keys/page.tsx
@@ -1,15 +1,15 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "AltuHealth Admin Messages",
-  description: "Manage support messages within the AltuHealth admin panel.",
+  title: "AltuHealth Admin API Keys",
+  description: "Manage API keys within the AltuHealth admin panel.",
 };
 
-export default function Messages() {
+export default function APIKeys() {
   return (
     <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
       <h3 className="mb-5 text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-7">
-        Support Messages
+        API Keys
       </h3>
       {/* Content goes here */}
     </div>
--- frontend/admin/src/app/(main)/(others)/notification/page.tsx
+++ frontend/admin/src/app/(main)/(others)/notification/page.tsx
@@ -1,15 +1,15 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "AltuHealth Admin Messages",
-  description: "Manage support messages within the AltuHealth admin panel.",
+  title: "AltuHealth Admin Notification Logs",
+  description: "View notification logs within the AltuHealth admin panel.",
 };
 
-export default function Messages() {
+export default function NotificationLogs() {
   return (
     <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
       <h3 className="mb-5 text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-7">
-        Support Messages
+        Notification Logs
       </h3>
       {/* Content goes here */}
     </div>
